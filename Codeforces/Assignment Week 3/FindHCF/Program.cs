9997b83f61cd2fbc6b60f5fa7f7a7c444814a297
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-01-30 18:19:50 +0530

    Weekly Assignment

---
+++ Codeforces/Assignment Week 3/FindHCF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FindHCF{
+    class Program{
+
+        public static int HCF(int a, int b){
+           return b == 0 ? a : HCF(b, a % b);
+        }
+        static void Main(){
+            int[] x = Console.ReadLine()
+                             .Split(' ')
+                             .Select(int.Parse)
+                             .ToArray();
+
+            int ans = HCF(x[0], x[1]);
+            Console.WriteLine(ans);
+        }
+    }
+}
