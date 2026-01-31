9340c1bd80c428131199e863b6a8abdfb617d33f
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-01-31 14:20:37 +0530

    CodeForces 96.A Football

---
+++ Codeforces/A.Football/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FootBall{
+    class Program{
+
+        public static bool Status(string pos){
+            int count = 0;
+            int team = int.Parse(pos[0].ToString());
+
+            if(pos.Length < 7){
+                return false;
+            }
+            for(int i = 0; i<pos.Length; i++){
+                if(count==7){
+                    return true;
+                }
+
+                if(int.Parse(pos[i].ToString()) == team){
+                    count++;
+                }
+                else{
+                    count = 1;
+                    team = int.Parse(pos[i].ToString());
+                }
+            }
+
+            return false;
+        }
+
+        static void Main(){
+            string positions = Console.ReadLine();
+            Console.WriteLine(Status(positions) ? "Yes" : "No");
+        }
+    }
+}
