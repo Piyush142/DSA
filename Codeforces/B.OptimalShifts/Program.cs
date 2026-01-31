10cfd5ab80442fcf8ddcc13136117fc4abe8c3bb
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-01-31 18:46:38 +0530

    OptimalShifts: Codeforces

---
+++ Codeforces/B.OptimalShifts/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OptimalShift{
+    class Program{
+
+        public static int CountMaxZeros(string st){
+            int count = 0;
+            int max = 0;
+            for(int i = 0; i<st.Length; i++){
+                if(int.Parse(st[i].ToString()) == 1){
+                    max = count>=max ? count : max;
+                    count = 0;
+                }
+                else{
+                    count++;
+                }
+            }
+            return max;
+        }
+        static void Main(){
+            var t = int.Parse(Console.ReadLine());
+            for(int i = 0; i<t; i++){
+                var len = int.Parse(Console.ReadLine());
+                var inputStr = Console.ReadLine();
+                inputStr += inputStr;
+                var count = CountMaxZeros(inputStr);
+                Console.WriteLine(count);
+            }
+
+        }
+    }
+}
