511c6db7100a0af3e746a878fd0a3000ea212323
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-02-17 21:26:47 +0530

    Leetcode: Soultion 3270 New approach

--- LeetCode/FindMissingNumbers/3270.cs
+++ LeetCode/FindMissingNumbers/3270.cs
@@ -1,35 +1,35 @@
-public class Solution {
+// public class Solution {
     
-    public int FindTarget(int[] nums, int target){
-        int left = 0;
-        int right = nums.Length - 1;
+//     public int FindTarget(int[] nums, int target){
+//         int left = 0;
+//         int right = nums.Length - 1;
         
-        while(left<=right){
-            int mid = left + (right - left)/2;
-            if(nums[mid] == target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                left = mid+1;
-            }
-            else{
-                right = mid - 1;
-            }
-        }
+//         while(left<=right){
+//             int mid = left + (right - left)/2;
+//             if(nums[mid] == target){
+//                 return mid;
+//             }
+//             else if(nums[mid]<target){
+//                 left = mid+1;
+//             }
+//             else{
+//                 right = mid - 1;
+//             }
+//         }
         
-        return -1;
-    }
-    public IList<int> FindDisappearedNumbers(int[] nums) {
-        IList<int> results = [];
+//         return -1;
+//     }
+//     public IList<int> FindDisappearedNumbers(int[] nums) {
+//         IList<int> results = [];
         
-        Array.Sort(nums);
+//         Array.Sort(nums);
         
-        for(int i = 1; i<=nums.Length; i++){
-            if(FindTarget(nums, i) == -1){
-                results.Add(i);
-            }
-        }
+//         for(int i = 1; i<=nums.Length; i++){
+//             if(FindTarget(nums, i) == -1){
+//                 results.Add(i);
+//             }
+//         }
         
-        return results; 
-    }
-}
+//         return results; 
+//     }
+// }
--- LeetCode/FindMissingNumbers/3270.ts
+++ LeetCode/FindMissingNumbers/3270.ts
@@ -1,30 +1,52 @@
-function findTarget(nums: number[], target: number): number {
-  let left = 0;
-  let right = nums.length - 1;
+// function findTarget(nums: number[], target: number): number {
+//   let left = 0;
+//   let right = nums.length - 1;
 
-  while (left <= right) {
-    const mid = left + (right - left) / 2;
-    if (nums[mid] === target) {
-      return mid;
-    } else if (nums[mid] > target) {
-      right = mid - 1;
-    } else {
-      left = mid + 1;
-    }
-  }
+//   while (left <= right) {
+//     const mid = left + (right - left) / 2;
+//     if (nums[mid] === target) {
+//       return mid;
+//     } else if (nums[mid] > target) {
+//       right = mid - 1;
+//     } else {
+//       left = mid + 1;
+//     }
+//   }
 
-  return -1;
-}
+//   return -1;
+// }
+
+// function findDisappearedNumbers(nums: number[]): number[] {
+//   nums.sort((a, b) => a - b);
+//   let results: number[] = [];
+
+//   for (let i = 1; i <= nums.length; i++) {
+//     if (findTarget(nums, i) === -1) {
+//       results.push(i);
+//     }
+//   }
+
+//   return results;
+// }
 
 function findDisappearedNumbers(nums: number[]): number[] {
-  nums.sort((a, b) => a - b);
-  let results: number[] = [];
+  let n = nums.length;
+  let i = 0;
+  let ans: number[] = [];
 
-  for (let i = 1; i <= nums.length; i++) {
-    if (findTarget(nums, i) === -1) {
-      results.push(i);
+  while (i < n) {
+    if (nums[i] === i + 1 || nums[nums[i] - 1] === nums[i]) {
+      i++;
+    } else {
+      let temp = nums[nums[i] - 1];
+      nums[nums[i] - 1] = nums[i];
+      nums[i] = temp;
     }
   }
 
-  return results;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== i + 1) ans.push(i + 1);
+  }
+
+  return ans;
 }
