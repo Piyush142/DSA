93aaa77aeb4a7e31bf00657617c5a4521677a99a
Piyush Chouhan <chouhanpiyush142@gmail.com>
2026-02-19 12:55:06 +0530

    Leetcode: Soultion 3270 New approach

--- LeetCode/FindMissingNumbers/3270.cs
+++ LeetCode/FindMissingNumbers/3270.cs
@@ -33,3 +33,27 @@
 //         return results; 
 //     }
 // }
+
+
+public class Solution {
+    public IList<int> FindDisappearedNumbers(int[] nums) {
+        var n = nums.Length;
+        var i = 0;
+        IList<int> ans = [];
+        while(i<n){
+            if(nums[i] == i+1 || nums[i] == nums[nums[i] - 1]){
+                i++;
+            }
+            else{
+                (nums[i], nums[nums[i]-1]) = (nums[nums[i] - 1], nums[i]);
+            }
+        }
+
+        for(int j = 0; j<nums.Length; j++){
+            if(nums[j] != j+1){
+                ans.Add(j+1);
+            }
+        }
+        return ans;
+    }
+}
